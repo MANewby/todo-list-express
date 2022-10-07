fe7c6b0c686f031809895afdc3e81f0f18e3bf08
MANewby <97006286+MANewby@users.noreply.github.com>
2022-10-06 23:10:18 -0700

    some changes almost done

--- server.js
+++ server.js
@@ -1,7 +1,8 @@
 const express = require('express')
 const app = express()
 const MongoClient = require('mongodb').MongoClient
-const PORT = 2121
+const PORT = 5500
+
 require('dotenv').config()
 
 
@@ -19,7 +20,6 @@ app.set('view engine', 'ejs')
 app.use(express.static('public'))
 app.use(express.urlencoded({ extended: true }))
 app.use(express.json())
-
 
 app.get('/',async (request, response)=>{
     const todoItems = await db.collection('todos').find().toArray()
